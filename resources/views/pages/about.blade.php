7988dfa0d438c071c49b962055ad314ef559fe83
Francky06 <ffroswell@gmx.fr>
2021-03-16 15:18:46 +0100

    init

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+@extends('base')
+
+@section('content')
+
+	<h1>Hello Freeks </h1>
+	<p>Oh Dear, it's time to sleep &hearts;</p>
+	<p><a href="http://localhost/first/public">Retour</a></p>
+
+
+@endsection
+
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('base')
+
+@section('content')
+
+	<h1>Hello World</h1>
+	<p>It's {{now()}} oh, Time to Code !!</p>	
+
+@endsection
