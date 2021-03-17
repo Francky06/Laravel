33ac175cfb5cd223cb23ad0eb9e673cb8dcd0845
Francky06 <ffroswell@gmx.fr>
2021-03-17 15:27:49 +0100

    init appli done

--- Test/resources/views/pages/about.blade.php
+++ Test/resources/views/pages/about.blade.php
@@ -7,7 +7,7 @@
 	<title>About</title>
 	<h1>Hello Freeks</h1>
 	<p>Oh Dear, it's time to sleep <span class="text-pink-500">&hearts;</span></p>
-	<img src="{{asset('images/color.jpg')}}" alt="Colors">
+	<img src="{{asset('images/color.jpg')}}" alt="Colors" class="shadow">
 	<p class="mt-5"><a href="{{ route('home') }}" class="text-indigo-500">Retour</a></p>
 @stop
 
