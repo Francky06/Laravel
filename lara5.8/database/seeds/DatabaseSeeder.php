b46d70e28709da09642509c814b0cd0f2e58c175
Francky06 <ffroswell@gmx.fr>
2021-03-23 15:29:19 +0100

    factories, seeders

--- lara5.8/database/seeds/DatabaseSeeder.php
+++ lara5.8/database/seeds/DatabaseSeeder.php
@@ -9,8 +9,10 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run() {
+        // appel seeders  
+        $this->call(UsersTableSeeder::class);
+        $this->call(EntreprisesTableSeeder::class);
+        $this->call(ClientsTableSeeder::class);
     }
 }
